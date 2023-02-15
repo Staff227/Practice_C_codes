c5792bfac04432c2de1e41626f1056226854129a
Staff227 <wnfynn@gmail.com>
2023-02-15 21:52:43 +0000

    understood an array

---
+++ arrays.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+
+
+int main (void)
+{
+
+               int numbers [30];
+	       
+               numbers [5] =23;
+               numbers [6] =10;
+
+               printf("%i", numbers [5] * numbers [6]);
+
+
+
+
+
+
+
+
+
+
+               return 0;
+}
